aade2abe8bec8429661ae3fb36565e17aa63f02a
De <spark49942@gmail.com>
2022-12-12 20:52:41 +0300

    DZ-f

--- proj.java
+++ proj.java
@@ -2,35 +2,37 @@
  * proj
  */
 import java.util.*;
-public final class proj {
+public final class proj 
+{
+    public static void main(String[] args) 
+    {
         Scanner work = new Scanner(System.in);
-    public static void main(String[] args, Scanner work) {
-        ArrayList<String> maci = new ArrayList<>();
-        maci.add("8 GB, 50.0 GB, Mac OS, white");
-        maci.add( "8 GB, 64.0 GB, Mac OS, green");
-        maci.add("8 GB, 100.0 GB, Linux, blue");
-        maci.add("16 GB, 1 TB, Windows 10, red");
-        maci.add("8 GB, 50.0 GB, Mac OS, white");
-        maci.add("16 GB, 84.0 GB, Windows 7, purple");
-        maci.add("7 GB, 50.0 GB, Linux, black");
-        Map<String, String> macc = new HashMap<>();
-        macc.put(" ", maci.get(0));
-        macc.put(" ", maci.get(1));
-        macc.put(" ", maci.get(2));
-        macc.put(" ", maci.get(3));
-        macc.put(" ", maci.get(4));
-        macc.put(" ", maci.get(5));
-        macc.put(" ", maci.get(6));
+        HashSet<String> mac = new HashSet<>();
+        mac.add("8 GB, 50.0 GB, Mac OS, white");
+        mac.add( "8 GB, 64.0 GB, Mac OS, green");
+        mac.add("8 GB, 100.0 GB, Linux, blue");
+        mac.add("16 GB, 1 TB, Windows 10, red");
+        mac.add("8 GB, 40.0 GB, Ubuntu, white");
+        mac.add("16 GB, 84.0 GB, Windows 7, purple");
+        mac.add("7 GB, 50.0 GB, Linux, black");
+        // Map<String, String> macc = new HashMap<>();
         System.out.println("Введите цифру, соответствующую необходимому критерию: ");
-        String key = work.nextLine();
-        if (key.equals("4"));
-            colors(work, maci);
+        filters(work, mac);
     }
-    public static void colors(Scanner work, ArrayList<String> lib) {
-        lib.get(0).split(",");
-        System.out.println(Arrays.asList(lib.get(0)));
-        System.out.println("Введите требуемый критерий по цвету:\n");
-        String clr = work.nextLine();
+    public static void filters(Scanner work, Set<String> lib) 
+    {
+        System.out.println("Введите требуемый критерии через пробел:");
+        String crit = work.nextLine();
+        for (String cha : lib) {
+            String[] crits = crit.split(" ");
+            String[] book = cha.split(", ");
+            for (String rac : book) 
+            {
+                if (crit.equals(rac)) {
+                    System.out.println(Arrays.toString(book));
+                }
+            }
+        }
         
     }
 }
